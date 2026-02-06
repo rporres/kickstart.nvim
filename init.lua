681c0e58689f63ce64a5973c0e751bb530b0e14f
Rafa Porres Molina <rporresm@redhat.com>
2026-02-07 00:22:43 +0100

    solarized colours
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- init.lua
+++ init.lua
@@ -749,25 +749,25 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  { -- Solarized colorscheme
+    'maxmx03/solarized.nvim',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      vim.o.background = 'dark'
+      require('solarized').setup {
+        transparent = { enabled = true },
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = true },
         },
+        palette = 'solarized',
+        variant = 'spring', -- higher contrast variant
+        on_highlights = function(colors, color)
+          return {
+            Normal = { fg = '#d4d4d4' }, -- brighter foreground
+          }
+        end,
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'solarized'
     end,
   },
 
@@ -821,9 +821,7 @@ require('lazy').setup({
       end
       vim.api.nvim_create_autocmd('FileType', {
         pattern = parsers,
-        callback = function()
-          pcall(vim.treesitter.start)
-        end,
+        callback = function() pcall(vim.treesitter.start) end,
       })
     end,
   },
